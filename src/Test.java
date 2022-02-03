7dc81e6a7b15e242d907d9020b37f9f4d89094af
Karnveer-Ubhi <k.ubhi@outlook.com>
2022-02-03 16:52:31 -0500

    Test for Karnveer Ubhi

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("hi guys");
-        System.out.println("Yo");
+        System.out.println("Nigeria");
     }
 }
