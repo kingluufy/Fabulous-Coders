230072cd692b13a7136f5f3deebde015b5b14ea5
Jeffrey Jiang <ouraize2003@gmail.com>
2022-02-03 17:52:46 -0500

    Functioning Game, in console only

---
+++ src/GameSettings.java
@@ -0,0 +1,44 @@
+public class GameSettings {
+    private int range;
+    private int tries;
+    private int answer;
+    private int guess;
+    GameSettings(int range, int tries, int answer) {
+
+        this.range = range;
+        this.tries = tries;
+        this.answer = answer;
+    }
+
+    public int getGuess() {
+        return guess;
+    }
+
+    public void setGuess(int guess) {
+        this.guess = guess;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
+
+    public int getTries() {
+        return tries;
+    }
+
+    public void setTries(int tries) {
+        this.tries = tries;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
+}
