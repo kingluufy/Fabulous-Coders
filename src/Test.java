1a5f76af0e7422fc82bb926c4398689fda35a727
kingluufy <prab2003@my.yorku.ca>
2022-02-03 17:15:51 -0500

    this is a test

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hi guys");
         System.out.println("Nigeria");
+        System.out.println("Welcome to the lands");
     }
 }
